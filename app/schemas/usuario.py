8159d6ddaa176c78248d94b7ee3d2f21e95119c5
HelgaZambrana <hvzambrana@gmail.com>
2025-05-16 20:54:41 -0300

    .

--- app/schemas/usuario.py
+++ app/schemas/usuario.py
@@ -2,29 +2,41 @@ from pydantic import BaseModel, EmailStr
 from enum import Enum
 from .ubicacion import UbicacionOut
 
+# Enum para roles
 class RolUsuario(str, Enum):
     usuario = "usuario"
     admin = "admin"
-# NOTA: Actualmente los roles están limitados a "usuario" y "admin" por simplicidad.
 
+# Para login
 class LoginRequest(BaseModel):
     email: EmailStr
     password: str
 
+# Token de acceso
 class TokenResponse(BaseModel):
     access_token: str
     token_type: str
 
+# Para futuros usos como update
 class UsuarioBase(BaseModel):
     nombre: str
     email: EmailStr
     telefono: str
     rol: RolUsuario
-    ubicacion_id: int
 
-class UsuarioCreate(UsuarioBase):
+# El esquema que se usa en POST /usuarios/registro
+class UsuarioCreate(BaseModel):
+    nombre: str
+    email: EmailStr
+    telefono: str
     password: str
+    rol: RolUsuario
+    direccion: str
+    codigo_postal: str
+    ciudad: str
+    provincia: str
 
+# Esquema de salida del usuario
 class UsuarioOut(BaseModel):
     id: int
     nombre: str
