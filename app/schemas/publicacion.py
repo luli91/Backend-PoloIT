7f09e87b9b1ee40651ef60cfd4d1bb0b9dbd43d0
HelgaZambrana <hvzambrana@gmail.com>
2025-05-16 19:14:00 -0300

    corregir errores publicaciones

--- app/schemas/publicacion.py
+++ app/schemas/publicacion.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from datetime import datetime
+from typing import Optional
 from app.schemas.estado import EstadoOut
 
 class PublicacionBase(BaseModel):
@@ -15,6 +16,11 @@ class PublicacionOut(PublicacionBase):
     id: int
     fecha_publicacion: datetime
     estado: EstadoOut
+
+class PublicacionUpdate(BaseModel):
+    imagen_url: Optional[str] = None
+    visible: Optional[bool] = None
+    estado_id: Optional[int] = None
 
     model_config = {
         "from_attributes": True
