3d4efaee7584213b078bc5ab29a2cfad91045776
HelgaZambrana <hvzambrana@gmail.com>
2025-05-17 01:22:18 -0300

    ubicacion

--- app/models/ubicacion.py
+++ app/models/ubicacion.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Column, Integer, String, Enum as SQLEnum
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from app.database import Base
 from app.utils.provincias import ProvinciaEnum
+from sqlalchemy import Enum as SQLEnum
 
 class Ubicacion(Base):
     __tablename__ = "ubicacion"
@@ -10,6 +11,6 @@ class Ubicacion(Base):
     direccion = Column(String)
     codigo_postal = Column(String)
     ciudad = Column(String)
-    provincia = Column(SQLEnum(ProvinciaEnum, name="provincia_enum"), nullable=False)
+    provincia = Column(SQLEnum(ProvinciaEnum, name="provincia_enum"))
 
     usuarios = relationship("Usuario", back_populates="ubicacion")
