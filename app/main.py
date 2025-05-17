40970282984ac4fad9c4fc0f3fb31b8d2de17676
HelgaZambrana <hvzambrana@gmail.com>
2025-05-16 23:11:49 -0300

    corregir seed

--- app/main.py
+++ app/main.py
@@ -1,31 +1,31 @@
 from dotenv import load_dotenv
 load_dotenv()
 
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import Base, engine
 from app.models import usuario, donacion, categoria, estado, ubicacion, publicacion
 from app.routers import usuarios, donaciones, publicaciones, categorias, estados, ubicaciones, ping
+from app.seed import cargar_categorias, cargar_estados
 
-# Crear tablas en la base de datos (porque no uso Alembic)
+# Crear tablas en la base de datos (solo si no usás Alembic)
 Base.metadata.create_all(bind=engine)
+
+# Activar o no el seed desde variable de entorno
+SEED_ACTIVO = os.getenv("SEED_ACTIVO", "0") == "1"
+print("Variable SEED_ACTIVO:", SEED_ACTIVO)
 
 app = FastAPI(
     title="API Donaciones",
     version="0.1.0"
 )
 
-# Solo ejecutar seeds si está habilitado con una variable
-import os
-from app.seed import cargar_categorias, cargar_estados
-
 @app.on_event("startup")
 def inicializar_datos():
-    print("SEED_ACTIVO =", os.getenv("SEED_ACTIVO"))  # DEBUG VISUAL
-
-    if os.getenv("SEED_ACTIVO") == "1":
-        print("Ejecutando seeds iniciales...")  # CONFIRMA QUE EL IF ENTRA
+    if SEED_ACTIVO:
+        print("Ejecutando seeds iniciales...")
         from app.database import SessionLocal
         db = SessionLocal()
         cargar_categorias(db)
@@ -35,7 +35,7 @@ def inicializar_datos():
 # Configurar CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  #Cambiar en producción
+    allow_origins=["*"],  # Cambiar esto en producción
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
