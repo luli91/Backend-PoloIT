8f5b5aa3b1bb9d8ab211cdfd7c4c24131a69b623
HelgaZambrana <hvzambrana@gmail.com>
2025-05-17 01:33:14 -0300

    estados

--- app/schemas/publicacion.py
+++ app/schemas/publicacion.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 from typing import Optional
-from app.utils.estado_nombre import EstadoNombre
+from app.utils.estado_nombre import EstadoNombreEnum
 
 class PublicacionCreate(BaseModel):
     mensaje: str
