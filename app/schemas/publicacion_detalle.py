e5c4f1ec08a8b110af007aceb18755c6e8a128b8
harkend <alfredo.villegas.hk@gmail.com>
2025-06-23 10:54:57 -0300

    fea(Publicacion):ReFactor campos no visibles.

--- app/schemas/publicacion_detalle.py
+++ app/schemas/publicacion_detalle.py
@@ -1,12 +1,17 @@
+from datetime import datetime
 from pydantic import BaseModel
 from app.utils.estado_nombre import EstadoNombreEnum
 from app.schemas.donacion import DonacionOut
 from app.schemas.usuario import UsuarioOut
+from typing import Optional
 
 class PublicacionDetalleOut(BaseModel):
     id: int
     mensaje: str
     estado: EstadoNombreEnum
+    fecha_publicacion: Optional[datetime]
+    imagen_url: Optional[str] = None
+    visible: Optional[bool] = None
     donacion: DonacionOut
     usuario: UsuarioOut
 
