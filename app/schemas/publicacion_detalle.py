f2019dd82555f666aa19100684df1a7b65ac84b9
harkend <alfredo.villegas.hk@gmail.com>
2025-06-28 18:15:51 -0300

    fea(Publicacion):ReFactor-Publicacion-UploadFile,Publiacion-visible-true,Update For Publicacion

--- app/schemas/publicacion_detalle.py
+++ app/schemas/publicacion_detalle.py
@@ -2,7 +2,6 @@ from datetime import datetime
 from pydantic import BaseModel
 from app.utils.estado_nombre import EstadoNombreEnum
 from app.schemas.donacion import DonacionOut
-from app.schemas.usuario import UsuarioOut
 from typing import Optional
 
 
